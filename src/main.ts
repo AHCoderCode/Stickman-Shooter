7cb214306d3063d31994e86f18ae03da478cdeb4
AHCoderCode <vision.ah13@gmail.com>
2026-08-15 12:35:13 +0500

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,7 @@
+import Phaser from 'phaser';
+import { gameConfig } from './config';
+import { Game } from './core';
 
+window.addEventListener('DOMContentLoaded', () => {
+  new Game(gameConfig);
+});
